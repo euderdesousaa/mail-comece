de3a2b4714f5685fd9b9b9f41a000490ebcd3250
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-05-21 10:22:44 -0300

    controller with a simple postmapping

---
+++ src/main/java/net/comece/smtpmailcomece/controller/EmailController.java
@@ -0,0 +1,32 @@
+package net.comece.smtpmailcomece.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.comece.smtpmailcomece.model.UserSenderEmail;
+import net.comece.smtpmailcomece.repository.UserSenderRepository;
+import net.comece.smtpmailcomece.service.EmailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService service;
+
+    private final UserSenderRepository repository;
+
+    @PostMapping
+    public ResponseEntity<Void> sender(@RequestBody @Valid UserSenderEmail user) {
+        service.emailsSender(user);
+        user.setDateAtCreate(LocalDate.now());
+        repository.save(user);
+        return ResponseEntity.ok().build();
+    }
+}
