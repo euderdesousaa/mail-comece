ffe7a6fce114caf730f303d75f2593e4712fc695
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-06-17 16:38:36 -0300

    segment

---
+++ src/main/java/net/comece/smtpmailcomece/model/enums/Segment.java
@@ -0,0 +1,18 @@
+package net.comece.smtpmailcomece.model.enums;
+
+public enum Segment {
+    SERVICOS,
+    VAREJO,
+    INDUSTRIA,
+    ECOMMERCE,
+    FOOD_SERVICE,
+    EDUCACAO,
+    IMOBILIARIA,
+    SAAS,
+    FINANCAS,
+    FRANQUIA,
+    TELECOM,
+    ENERGIA_SOLAR,
+    TURISMO,
+    OUTROS
+}
