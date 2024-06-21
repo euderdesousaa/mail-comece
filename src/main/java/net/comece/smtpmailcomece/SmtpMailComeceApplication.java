1fa35138c37872d38326bacdf54af39eabbdeb24
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-06-21 16:23:18 -0300

    Spring cofing

--- src/main/java/net/comece/smtpmailcomece/SmtpMailComeceApplication.java
+++ src/main/java/net/comece/smtpmailcomece/SmtpMailComeceApplication.java
@@ -2,11 +2,12 @@ package net.comece.smtpmailcomece;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableAsync
 @EnableScheduling
 @ComponentScan({"net.comece.smtpmailcomece.controller", "net.comece.smtpmailcomece.repository", "net.comece.smtpmailcomece.service" })
